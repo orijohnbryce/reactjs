89004c3171e8f9a7ba73be22970b0a8af6d528a9
Ori Brook <ori@gmail.com>
2025-07-03 19:17:25 +0300

    add challenges

--- src/Bitcoin/Bitcoin.jsx
+++ src/Bitcoin/Bitcoin.jsx
@@ -1,29 +1,40 @@
 import { useEffect, useState } from "react"
 import { getBitcoinPrice } from "../client";
+import Spinner from "../Spinner/Spinner";
 
-const Bitcoin = () => {
+const Bitcoin = ({ x = 5 }) => {
     const [price, setPrice] = useState();
+    const [showSpinner, setShowSpinner] = useState(false)
 
     const fetchPrice = async () => {
+        setShowSpinner(true)
         const updatedPrice = await getBitcoinPrice();
+        setShowSpinner(false)
         setPrice(updatedPrice);
+        document.title = updatedPrice;
     }
-    
-    useEffect(() => {        
+
+    useEffect(() => {
         // getBitcoinPrice().then((updatedPrice) => {
         //     setPrice(updatedPrice);
-        // }).catch((e) => alert("Error fetching price"));        
+        // }).catch((e) => alert("Error fetching price"));      
+        
         fetchPrice();
         document.title = "ביטקוין";
+
+        setInterval(() => {
+            fetchPrice();
+        }, x * 1000)
     }, [])
+    console.log(showSpinner);
 
     return (<>
-        {!price && <div>Loading... </div>}
+        {/* {!price && <div>Loading... </div>} */}
+        {showSpinner && <Spinner />}
 
         {price && <div>
-            Bitcoin price: {price} 
-            
-            </div>}
+            Bitcoin price: {price}
+        </div>}
 
     </>
     )
