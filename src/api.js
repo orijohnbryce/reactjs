f898e2792a237a17f33f4c0ca167701cd1a049db
Ori Brook <ori@gmail.com>
2025-07-14 21:07:40 +0300

    save

--- src/api.js
+++ src/api.js
@@ -15,3 +15,37 @@ export async function updateProduct(productId, product) {
     const url = `http://localhost:3030/api/products/${productId}`
     await axios.put(url, product);
 }
+
+export async function register(firstName, lastName, email, password) {
+    const url = "http://localhost:3030/api/register/"
+    const data = {
+        firstName,
+        lastName,
+        email,
+        password
+    }
+
+    try {        
+        const res = await axios.post(url, data);
+        console.log(res);
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+export async function login(email, password) {
+    const url = "http://localhost:3030/api/login/"
+    try {
+        
+        const data = {email, password}
+        const res = await axios.post(url, data);
+        const token = res.data;
+        return token;
+
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+// register('david11',"cohen11", "david00542555@gmail.com", "1234")
+login("bart@gmail.com", "123456")
