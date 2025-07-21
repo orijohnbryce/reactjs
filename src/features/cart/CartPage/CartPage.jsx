5120a97b00967f27794b80d64c684f64cdf1e842
Ori Brook <ori@gmail.com>
2025-07-21 20:56:16 +0300

    save

--- src/features/cart/CartPage/CartPage.jsx
+++ src/features/cart/CartPage/CartPage.jsx
@@ -1,18 +1,21 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../../App'
 import "./CartPage.css"
+import ProductCard from '../../products/ProductCard/ProductCard';
 
 const CartPage = () => {
 
-    const {cart, setCart} = useContext(AppContext);
-  return (
-    <div>
-        {cart.map((p)=><div id='cart-page-single-product-container'>
-            <span>{p.data.name}</span>
-            <span>{p.amount}</span>            
-        </div>)}
-    </div>
-  )
+    const { cart, setCart } = useContext(AppContext);
+    return (
+        <div>
+            {cart.map((p) => <ProductCard> <div id='cart-page-single-product-container'>
+                <span>{p.data.name}</span>
+                <span>{p.amount}</span>
+            </div>
+            </ProductCard>
+            )}
+        </div>
+    )
 }
 
 export default CartPage
